a12c5663d56f65be27e6c85f161671f093b52c7c
iwonasatro <iwona.satro@gmail.com>
2020-10-14 13:15:47 +0200

    custom form control

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,27 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { UserEditComponent } from '../user-edit/user-edit.component';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
-
-
-  constructor(public dialog: MatDialog) {}
-
+  customForm: FormGroup;
+  constructor(public dialog: MatDialog, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.customForm = this.fb.group({
+      customCheckbox: new FormControl(true),
+    });
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(UserEditComponent, {
-
-      data: {}
+      data: {},
     });
-
   }
-
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UserComponent } from '../components/user/user.component';
+import { CustomFormControlComponent } from '../shared/custom-form-control/custom-form-control.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [CommonModule, UsersRoutingModule],
+  imports: [CommonModule, UsersRoutingModule, ReactiveFormsModule],
+  entryComponents: [CustomFormControlComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class UsersModule {}
