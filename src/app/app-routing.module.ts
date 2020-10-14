39d99a89f6d65de497d470e73063b6cd8053d75a
Tomasz Lukaszyk <tomasz.lukaszyk@avanade.com>
2020-10-14 12:20:03 +0200

    Add can load guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { AdminComponent } from './components/admin/admin.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { AuthGuard } from './guards/auth.guard';
+import { UsersGuard } from './guards/users.guard';
 
 const routes: Routes = [
   { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   {
     path: '',
-    children: [{ path: 'users', loadChildren: () => import('./users/users.module').then((module) => module.UsersModule) }],
+    children: [
+      { path: 'users', loadChildren: () => import('./users/users.module').then((module) => module.UsersModule), canLoad: [UsersGuard] },
+    ],
   },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: '**', component: PageNotFoundComponent },
